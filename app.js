f182fa79b580ef7d2f750819134f9087634dc2ce
Mohammad Aadil <233125@kit.ac.in>
2026-02-23 14:30:55 +0530

    Fixed mobile start issue in Simon game

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let level=0;
 let h2 = document.querySelector("h2");
 let btns = ["yellow", "red", "purple", "green"]
 
-document.addEventListener("keypress", function(){
+document.addEventListener("click", function(){
     if(started == false){
         console.log("game started!");
         started = true;
